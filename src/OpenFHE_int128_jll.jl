496e6e46bd5d30be8d621ec37dc4e7d9e14c168e
jlbuild <juliabuildbot@gmail.com>
2026-04-13 16:32:41 +0000

    OpenFHE_int128_jll build 1.5.0+0

--- src/OpenFHE_int128_jll.jl
+++ src/OpenFHE_int128_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("OpenFHE_int128")
-JLLWrappers.@generate_main_file("OpenFHE_int128", UUID("a89a0bdd-1663-5679-8b21-c3a5388322bc"))
+JLLWrappers.@generate_main_file("OpenFHE_int128", Base.UUID("a89a0bdd-1663-5679-8b21-c3a5388322bc"))
 end  # module OpenFHE_int128_jll
